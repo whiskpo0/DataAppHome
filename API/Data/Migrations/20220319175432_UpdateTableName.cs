bb76c7ed3a1b71d208cc7389a625252b02e10ff5
whisk <whiskypo082@gmail.com>
2022-03-19 13:11:25 -0500

    Added DB Connetion for Users

---
+++ API/Data/Migrations/20220319175432_UpdateTableName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UpdateTableName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppUsers",
+                table: "AppUsers");
+
+            migrationBuilder.RenameTable(
+                name: "AppUsers",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "AppUsers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppUsers",
+                table: "AppUsers",
+                column: "Id");
+        }
+    }
+}
