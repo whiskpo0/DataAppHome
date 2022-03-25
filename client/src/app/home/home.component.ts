7d3bc255194a5126df6e38bede86bf2343ea67da
whisk <whiskypo082@gmail.com>
2022-03-24 22:50:41 -0500

    trying to get dropsown to work.. url is wrong.

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -9,15 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   registerMode = false; 
+  users: any; 
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getUsers(); 
   }
 
   registerToggle()
   {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers()
+  { 
+    this.http.get('https:/localhost:5001/api/users').subscribe(users => this.users = users);
   }
 
 }
